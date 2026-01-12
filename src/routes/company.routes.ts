e3bc33ef2f548c2e1458b1909f7e05ba32e86752
Manuel Garderes <69734460+manugarderes@users.noreply.github.com>
2026-01-12 17:47:47 -0300

    route added

--- src/routes/company.routes.ts
+++ src/routes/company.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware/auth.js";
-import { getMine } from "../controllers/company.controller.js";
+import { getMetrics, getMine } from "../controllers/company.controller.js";
 
 const router = Router();
 
 router.use(authMiddleware);
 router.get("/", getMine);
+router.get("/metrics", getMetrics);
 
 export default router;
